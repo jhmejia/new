aaca54f0b664b7f5e0477a143d8f638a8a19a963
John Henry Mejia <jmejia@atlassian.com>
2023-08-03 08:57:30 -0500

    add main js

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,4 +35,26 @@
 
 // };
 
+(function ($) {
+    var s,
+        spanizeLetters = {
+            settings: {
+                letters: $('.js-spanize'),
+            },
+            init: function () {
+                s = this.settings;
+                this.bindEvents();
+            },
+            bindEvents: function () {
+                s.letters.html(function (i, el) {
+                    //spanizeLetters.joinChars();
+                    var spanizer = $.trim(el).split("");
+                    return '<span>' + spanizer.join('</span><span>') + '</span>';
+                });
+            },
+        };
+    spanizeLetters.init();
+})(jQuery);
+
+
 document.addEventListener('DOMContentLoaded', app.init);
